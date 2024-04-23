db8f000975c4fec80815f8eb23cadd1c3d0844bd
mhtwebdev <farzanakhtarf99@gmail.com>
2024-04-23 11:05:49 +0700

    finished

--- src/main.py
+++ src/main.py
@@ -11,71 +11,93 @@ from config import CHROME_PROFILE_PATH
 try:
     if sys.argv[1]:
         with open(sys.argv[1], 'r', encoding='utf8') as f:
-            groups = [group.strip() for group in f.readlines()]
+            numbers = [number.strip() for number in f.readlines()]
 except IndexError:
-    print('Please provide the group name as the first argument.')
+    print('Please provide the number list as the first argument.')
 
 with open('msg.txt', 'r', encoding='utf8') as f:
     msg = f.read()
 
-options = webdriver.ChromeOptions()
-options.add_argument(CHROME_PROFILE_PATH)
-
-browser = webdriver.Chrome(
-    executable_path='/Users/akjasim/chromedriver/chromedriver', options=options)
-
+browser = webdriver.Chrome()
 browser.maximize_window()
 
 browser.get('https://web.whatsapp.com/')
 
-for group in groups:
-    search_xpath = '//div[@contenteditable="true"][@data-tab="3"]'
+input("Press ENTER after login into Whatsapp Web and your chats are visible.")
 
-    search_box = WebDriverWait(browser, 500).until(
-        EC.presence_of_element_located((By.XPATH, search_xpath))
-    )
+added_numbers = []
 
-    search_box.clear()
+# Load added numbers from /contacted.txt
+with open('contacted.txt', 'r', encoding='utf8') as f:
+    contacts = f.read().splitlines()
+    added_numbers = contacts
 
-    time.sleep(1)
+for number in numbers:
+    if number in added_numbers:
+        continue
+    else:
+        new_chat_btn = browser.find_element(by=By.XPATH, value='//span[@data-icon="new-chat-outline"]')
+        
+        time.sleep(1)
+        
+        new_chat_btn.click()
+        
+        try:
+            search_css_selector = '.selectable-text.copyable-text.x15bjb6t.x1n2onr6'
 
-    pyperclip.copy(group)
+            search_box = WebDriverWait(browser, 500).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, search_css_selector))
+            )
 
-    search_box.send_keys(Keys.SHIFT, Keys.INSERT)  # Keys.CONTROL + "v"
+            search_box.clear()
 
-    time.sleep(2)
-
-    group_xpath = f'//span[@title="{group}"]'
-    group_title = browser.find_element_by_xpath(group_xpath)
-
-    group_title.click()
-
-    time.sleep(1)
-
-    input_xpath = '//div[@contenteditable="true"][@data-tab="1"]'
-    input_box = browser.find_element_by_xpath(input_xpath)
-
-    pyperclip.copy(msg)
-    input_box.send_keys(Keys.SHIFT, Keys.INSERT)  # Keys.CONTROL + "v"
-    input_box.send_keys(Keys.ENTER)
-
-    time.sleep(1)
-
-    try:
-        if sys.argv[2]:
-            attachment_box = browser.find_element_by_xpath(
-                '//div[@title="Attach"]')
-            attachment_box.click()
             time.sleep(1)
 
-            image_box = browser.find_element_by_xpath(
-                '//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
-            image_box.send_keys(sys.argv[2])
+            pyperclip.copy(number)
+
+            search_box.send_keys(Keys.SHIFT, Keys.INSERT)  # Keys.CONTROL + "v"
+
             time.sleep(2)
 
-            send_btn = browser.find_element_by_xpath(
-                '//span[@data-icon="send"]')
-            send_btn.click()
-            time.sleep(2)
-    except IndexError:
-        pass
+            chat_css_selector = '._ak72._ak73'
+            chat_title = browser.find_element(by=By.CSS_SELECTOR, value=chat_css_selector)
+
+            chat_title.click()
+
+            time.sleep(1)
+
+            try:
+                if sys.argv[2]:
+                    attachment_box = browser.find_element(by=By.XPATH, value='//div[@title="Attach"]')
+                    attachment_box.click()
+                    time.sleep(1)
+
+                    image_box = browser.find_element(by=By.XPATH, value='//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+                    image_box.send_keys(sys.argv[2])
+                    time.sleep(2)
+
+                    send_btn = browser.find_element(by=By.XPATH, value='//span[@data-icon="send"]')
+                    send_btn.click()
+                    time.sleep(2)
+            except IndexError:
+                pass
+
+            input_xpath = '//div[@contenteditable="true"][@role="textbox"][@title="Type a message"]'
+            input_box = browser.find_element(by=By.XPATH, value=input_xpath)
+
+            pyperclip.copy(msg)
+            input_box.send_keys(Keys.SHIFT, Keys.INSERT)  # Keys.CONTROL + "v"
+            input_box.send_keys(Keys.ENTER)
+            
+            added_numbers.append(number)
+
+            time.sleep(1)
+        except:
+            back_btn = browser.find_element(by=By.XPATH, value='//div[@role="button"][@aria-label="Back"]')
+            back_btn.click()
+            pass
+        
+# Save added_numbers to /contacted.txt
+with open('contacted.txt', 'w', encoding='utf8') as f:
+    for number in added_numbers:
+        f.write(f'{number}\n')
